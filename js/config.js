f8811c5f76340ea6edb4ef46d879466712a1b0fa
qu926 <koharu.vup@gmail.com>
2026-05-02 16:20:55 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.ABYSS_CONFIG = {
   storageMode: "supabase",
-  supabaseUrl: "https://cdnbkbryksrhioajgorg.supabase.co/rest/v1/",
+  supabaseUrl: "https://cdnbkbryksrhioajgorg.supabase.co",
   supabaseAnonKey: "sb_publishable_d-ydLZw9k8vNPpDnu_QDGA_ACjkGL_i",
   stateRowId: "host-event-manager",
 };
